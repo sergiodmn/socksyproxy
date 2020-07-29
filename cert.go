2a1f552d8d7e6f0eb947bd64525c8ef05a80bd8c
Sergio de Miguel <sergio.demiguel@nccgroup.com>
2020-07-29 11:28:10 +0200

    change to go modules and update go 1.14

---
+++ cert.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
+	"fmt"
+
+	"github.com/elazarl/goproxy"
+)
+
+func setCA(caCert, caKey []byte) error {
+	goproxyCa, err := tls.X509KeyPair(caCert, caKey)
+	if err != nil {
+		return err
+	}
+	if goproxyCa.Leaf, err = x509.ParseCertificate(goproxyCa.Certificate[0]); err != nil {
+		return err
+	}
+	goproxy.GoproxyCa = goproxyCa
+	goproxy.OkConnect = &goproxy.ConnectAction{Action: goproxy.ConnectAccept, TLSConfig: goproxy.TLSConfigFromCA(&goproxyCa)}
+	goproxy.MitmConnect = &goproxy.ConnectAction{Action: goproxy.ConnectMitm, TLSConfig: goproxy.TLSConfigFromCA(&goproxyCa)}
+	goproxy.HTTPMitmConnect = &goproxy.ConnectAction{Action: goproxy.ConnectHTTPMitm, TLSConfig: goproxy.TLSConfigFromCA(&goproxyCa)}
+	goproxy.RejectConnect = &goproxy.ConnectAction{Action: goproxy.ConnectReject, TLSConfig: goproxy.TLSConfigFromCA(&goproxyCa)}
+	return nil
+}
+
+func cert() []byte {
+    c, err := ioutil.ReadFile("ca.pem")
+    if err != nil {
+        fmt.Print(err)
+    }
+    return []byte(c)
+}
+
+func key() []byte {
+    k, err := ioutil.ReadFile("ca.key.pem")
+    if err != nil {
+        fmt.Print(err)
+    }
+    return []byte(k)
+}
+
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/sergiodmn/socksyproxy
+
+go 1.14
+
+require (
+	github.com/elazarl/goproxy v0.0.0-20200710112657-153946a5f232
+	golang.org/x/net v0.0.0-20200707034311-ab3426394381
+)
